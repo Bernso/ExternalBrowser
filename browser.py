faa0bff4994393f96bd36eff76429c62ad85e321
benwsheridan <ben.w.sheridan@gmail.com>
2024-04-25 20:40:05 +0100

    yes

--- browser.py
+++ browser.py
@@ -1,17 +1,22 @@
 import customtkinter as tk
 import webbrowser
+from tkinter import messagebox
 
 def open_url():
-    url = entry.get()
-    webbrowser.open_new(url)
-
+    url = entry.get().strip()
+    if url:
+        try:
+            webbrowser.open_new(url)
+        except Exception as e:
+            messagebox.showerror("Error", f"Failed to open URL: {e}")
+    else:
+        messagebox.showwarning("Warning", "Please enter a valid URL")
 
 root = tk.CTk()
 root.geometry('450x100')
 root.title("External Web Browser")
 
-
-entry = tk.CTkEntry(root, placeholder_text='Enter your link here', width=350)
+entry = tk.CTkEntry(root, width=350, placeholder_text='Enter your link here: ')
 entry.pack(pady=10)
 
 button = tk.CTkButton(root, text="Open URL", command=open_url)
